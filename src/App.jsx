5c7d6ec7e2ab3f4ef3bfc299e62e62cb54c368b1
Hamdan Salih <hamdansalihpc@gmail.com>
2023-11-17 08:52:14 +0530

    modified App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate, Router} from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate, } from 'react-router-dom'
 import './App.css'
 
 import AuthForm from './pages/account/AuthForm'
@@ -10,6 +10,7 @@ import Register from './pages/account/Register'
 import StudRegister from './pages/account/StudRegister'
 import ProvRegister from './pages/account/ProvRegister'
 import CheckAuth from './utilities/CheckAuth'
+import Profile from './pages/adminpanel/Profile'
 
 
 
@@ -23,7 +24,7 @@ function App() {
           {/* Admin Routes ..testing */}
           <Route path='/account' exact element={<AuthForm />}>
             <Route index element={<Navigate to='login' />} ></Route>
-            <Route path='login' exact element={<CheckAuth><Login /></CheckAuth>}></Route>
+            <Route path='login' exact element={<CheckAuth role={"login"} name={'login'}><Login /></CheckAuth>}></Route>
             <Route path='register' exact element={<Register />}>
               <Route index element={<Navigate to='student' />} ></Route>
               <Route path='student' exact element={<StudRegister />}></Route>
@@ -32,11 +33,12 @@ function App() {
 
           </Route>
 
-          <Route path='/admin' exact element={<AdminHome/>}>
+          <Route path='/admin' exact element={<CheckAuth><AdminHome/></CheckAuth>}>
 
             <Route index element={<Navigate to='dashboard' />} ></Route>
             <Route path='dashboard' exact element={<Dashboard />}></Route>
             <Route path='users' exact element={<UsersList />}></Route>
+            <Route path='profile' exact element={<Profile />}></Route>
 
           </Route>
           
