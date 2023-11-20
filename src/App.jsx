d1a683ba7fcbdf6bd705c302e7141989f3363c07
Hamdan Salih <hamdansalihpc@gmail.com>
2023-11-20 16:18:31 +0530

    update main

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,27 @@ import StudRegister from './pages/account/StudRegister'
 import ProvRegister from './pages/account/ProvRegister'
 import CheckAuth from './utilities/CheckAuth'
 import Profile from './pages/adminpanel/Profile'
+import Courses from './pages/adminpanel/Courses'
+import Plans from './pages/adminpanel/Plans'
+import Providers from './pages/adminpanel/Providers'
+import ProviderHome from './pages/providers/ProviderHome'
+import ProviderDashboard from './pages/providers/ProviderDashboard'
+import ProviderProfile from './pages/providers/ProviderProfile'
+import ProviderCourses from './pages/providers/ProviderCourses'
+import ProviderPlan from './pages/providers/ProviderPlan'
+import ProviderQuestions from './pages/providers/ProviderQuestions'
+import StudentPage from './pages/students/StudentPage'
+import StudHome from './pages/students/StudHome'
+import StudProfile from './pages/students/StudProfile'
 
 
 
 
 function App() {
+
+  const adminMenuItems = ['dashboard' , 'courses' , 'users' , 'providers' ,'plans' , 'profile']
+  const providerMenuItems = ['dashboard' , 'courses' , 'questions' , 'plans' , 'profile']
+
 
   return (
     <>
@@ -24,22 +40,37 @@ function App() {
           {/* Admin Routes ..testing */}
           <Route path='/account' exact element={<AuthForm />}>
             <Route index element={<Navigate to='login' />} ></Route>
-            <Route path='login' exact element={<CheckAuth role={"login"} name={'login'}><Login /></CheckAuth>}></Route>
+            <Route path='login' exact element={<CheckAuth role={"login"} from={'login'}><Login /></CheckAuth>}></Route>
             <Route path='register' exact element={<Register />}>
               <Route index element={<Navigate to='student' />} ></Route>
               <Route path='student' exact element={<StudRegister />}></Route>
               <Route path='provider' exact element={<ProvRegister />}></Route>
             </Route>
-
           </Route>
 
-          <Route path='/admin' exact element={<CheckAuth><AdminHome/></CheckAuth>}>
-
+          <Route path='/admin' exact element={<CheckAuth from={'admin'}><AdminHome menus={adminMenuItems} /></CheckAuth>}>
             <Route index element={<Navigate to='dashboard' />} ></Route>
             <Route path='dashboard' exact element={<Dashboard />}></Route>
+            <Route path='courses' exact element={<Courses />}></Route>
             <Route path='users' exact element={<UsersList />}></Route>
+            <Route path='providers' exact element={<Providers />}></Route>
+            <Route path='plans' exact element={<Plans />}></Route>
             <Route path='profile' exact element={<Profile />}></Route>
+          </Route>
 
+          <Route path='/provider' exact element={<CheckAuth from={'provider'}><ProviderHome menus={providerMenuItems} /></CheckAuth>}>
+            <Route index element={<Navigate to='dashboard' />} ></Route>
+            <Route path='dashboard' exact element={<ProviderDashboard />}></Route>
+            <Route path='profile' exact element={<ProviderProfile />}></Route>
+            <Route path='courses' exact element={<ProviderCourses />}></Route>
+            <Route path='plans' exact element={<ProviderPlan />}></Route>
+            <Route path='questions' exact element={<ProviderQuestions />}></Route>
+          </Route>
+
+          <Route path='/student' exact element={<CheckAuth from={'student'}><StudentPage /></CheckAuth>}>
+            <Route index element={<Navigate to='home' />} ></Route>
+            <Route path='home' exact element={<StudHome />}></Route>
+            <Route path='profile' exact element={<StudProfile />}></Route>
           </Route>
           
       </Routes>
