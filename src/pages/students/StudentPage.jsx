71da6e4c17dead4dd719324b399080110e29c42a
Hamdan Salih <hamdansalihpc@gmail.com>
2023-12-03 10:07:57 +0530

    student profile

--- src/pages/students/StudentPage.jsx
+++ src/pages/students/StudentPage.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Navbar from '../partials/Navbar'
 import { Outlet } from 'react-router-dom'
+import './studentpage.css'
 
 function StudentPage() {
   return (
-    <div className='border-2 border-red-900 bg-black'>
+    <div className='border-2 border-red-900'>
       <Navbar />
       <div className='px-36 py-12 h-full h-screen'>
         <Outlet />
