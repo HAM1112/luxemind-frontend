b5c933bf2f14abcb7822f87be2e3b5e59ae703e6
Hamdan Salih <hamdansalihpc@gmail.com>
2023-12-04 10:10:13 +0530

    student profile

--- src/pages/students/components/Banner.jsx
+++ src/pages/students/components/Banner.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './banner.css'
 import discussion from '../../../assets/discussion.png'
+import { SpinnerDiamond } from 'spinners-react';
 
 function Banner() {
+  const [bannerImage, setBannerImage] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setBannerImage(discussion)
+    setLoading(false)
+  }, []);
+
   return (
+
+    <>
+        {loading ? 
+          <SpinnerDiamond size={70} thickness={84} speed={101} color="rgba(57, 145, 172, 1)" secondaryColor="rgba(0, 0, 0, 1)" />
+          :
+
+
     <div className='banner border-2 border-red-600 relative'>
       <div className='absolute h-full border-2 border-green-600 top-0 right-72'>
         <img src={discussion} alt="" />
@@ -20,6 +36,8 @@ function Banner() {
         <p className='text-sm'>Explore all courses</p>
       </div>
     </div>
+        }
+    </>
   )
 }
 
--- src/pages/students/components/ProfileUpdate.jsx
+++ src/pages/students/components/ProfileUpdate.jsx
@@ -1,65 +1,141 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './profileupdate.css'
 import accoPic from '../../../assets/acco.png'
 import uploadPic from '../../../assets/fileupload.png'
+import { firebasestore } from '../../../firebase/firebase'
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage'
+import api from '../../../api/api'
+import useUrlHeader from '../../../utilities/urlHeader'
+
+'use client';
+
 
 function ProfileUpdate(props) {
     console.log(props.userDetails);
     const [user, setUser] = useState(props.userDetails);
+    const [image, setImage] = useState(null);
+    const fileInputRef = useRef(null);
+    const header = useUrlHeader()
+
+    useEffect(() => {
+        const input = document.getElementById('dob');
+        const today = new Date();
+        today.setFullYear(today.getFullYear() - 5);
+        const formattedDate = today.toISOString().split('T')[0];
+        input.setAttribute('max', formattedDate);
+
+    }, []);
     
     const handleChange = (e) =>{
         setUser({ ...user, [e.target.name]: e.target.value})
     }
+    const handlePicRemove = ()=> {
+        setUser(prev => ({...prev,avatar:''}))
+    }
+    
+    const handleFileUpload = () => {
+        console.log('clicked');
+        // fileInputRef.current.click()
+        if(image == null){
+            console.log("upload btn");
+            return
+        }
+        const imgRef = ref(firebasestore,`files/${user.username}`)
+        uploadBytes(imgRef , image)
+        .then((snapshot) => {
+                getDownloadURL(snapshot.ref).then(url => {
+                    setUser((prev) => ({...prev, avatar : url}))
+                    console.log('image uploaded successfully');
+                })
+            }).catch(error => {
+                console.log("snapshot error");
+        }).catch(error => {
+            console.log('upload error');
+        })
+        setImage(null)
+    } 
 
-    const uploadFiles = ( file ) => {
-        //
-        if (!file) return;
+    const handleSave = () => {
+        console.log(user);
+        // api.put(
+        //     '/student/update-profile',
+        //     header,
+        //     user
+        // )
     }
 
+
+
   return (
+
     <div className='update-card'>
-    {console.log(user)}
-    {console.log(accoPic)}
+    {/* {console.log(getTodayMinus5Years())} */}
         <div className='grid gap-3 mb-3 justify-center'>
-            <div className='border-2 border-blue-600'>
-                <div className='group relative border-2 border-green-700 w-fit'>
-                    <img src={user.avatar ? user.avatar : accoPic} alt={user.username} className='rounded-full' />
-                    <div className='image-upload rounded-full hidden group-hover:flex absolute top-0 h-full justify-center items-center'>
-                        <img src={uploadPic} alt="upload pic" className='w-2/4 h-2/4'/>
+            <div className=''>
+                <div className='group relative w-56 h-56'>
+                    <img src={user.avatar ? user.avatar : accoPic} alt={user.username} className='rounded-full w-full h-full' />
+                    <div className='image-upload p-10 rounded-full hidden group-hover:flex absolute top-0 h-full justify-center items-center' 
+                        onClick={()=>fileInputRef.current.click()}>
+                        <img src={uploadPic} alt="upload pic" className='w-full h-full'/>
                     </div>
                 </div>
-                <div>
-                    <input type="file" className='input'/><br />
-
-                    <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' >Upload</button>
+                <div className='flex justify-center mt-3 w-full'>
+                    <input 
+                    type="file" 
+                    className='input hidden' 
+                    id='image_input'
+                    ref={fileInputRef}
+                    onChange={(e)=>{
+                        setImage(e.target.files[0])
+                        console.log('changed')
+                        handleFileUpload()
+                    }}/><br />
+                    {/* <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' onClick={()=>fileInputRef.current.click()}>Upload</button> */}
+                    <button className='button-54 bg-red-500' onClick={handlePicRemove}>Remove</button>
+                    
                 </div>
             </div>
         </div>
 
         <div className='grid gap-3 mb-3'>
             <label className='font-semibold'>Username</label>
-            <input onChange={handleChange} type="text" name='username' value={user.username} className="rounded-[7px] border-2 px-2 py-1 outline-none focus:ease-in duration-150 focus:ring-2 focus:ring-slate-900"/>
+            <input onChange={handleChange} type="text" name='username' value={user.username} className="border-2 px-2 py-1 outline-none focus:ease-in duration-150 focus:ring-2 focus:ring-slate-900"/>
         </div>
         <div className='flex justify-evenly gap-4'>
             <div className='grid gap-3 mb-3 w-full'>
                 <label className='font-semibold'>First name</label>
-                <input onChange={handleChange} type="text" name='first_name' value={user.first_name} className="rounded-[7px] border-2 px-2 py-1 outline-none focus:ease-in duration-150 focus:ring-2 focus:ring-slate-900"/>
+                <input onChange={handleChange} type="text" name='first_name' value={user.first_name} className="border-2 px-2 py-1 outline-none focus:ease-in duration-150 focus:ring-2 focus:ring-slate-900"/>
             </div>
             <div className='grid gap-3 mb-3 w-full'>
                 <label className='font-semibold'>Second name</label>
-                <input onChange={handleChange} type="text" name='last_name' value={user.last_name} className="rounded-[7px] border-2 px-2 py-1 outline-none focus:ease-in duration-150 focus:ring-2 focus:ring-slate-900"/>
+                <input onChange={handleChange} type="text" name='last_name' value={user.last_name} className="border-2 px-2 py-1 outline-none focus:ease-in duration-150 focus:ring-2 focus:ring-slate-900"/>
             </div>
         </div>
-        <div className='grid gap-3 mb-3'>
-            <label className='font-semibold'>Username</label>
-            <input onChange={handleChange} type="text" className="rounded-[7px] border-2 px-2 py-1 outline-none focus:ease-in duration-150 focus:ring-2 focus:ring-slate-900"/>
+        <div className='flex justify-evenly gap-4'>
+            <div className='grid gap-3 mb-3 w-7/12 '>
+                <label className='font-semibold'>Education</label>
+                <input onChange={handleChange} type="text" name='education' value={user.education} className="border-2 px-2 py-1 outline-none focus:ease-in duration-150 focus:ring-2 focus:ring-slate-900"/>
+            </div>
+            <div className='grid gap-3 mb-3 w-5/12'>
+                {/* <DatePicker label="Basic date picker" /> */}
+                {/* <Datepicker /> */}
+                
+                <label className='font-semibold'>Date of Birth</label>
+                <input type="date" id='dob' name='dob' onChange={handleChange} value={user.dob} className='border-2 px-2 py-1 outline-none focus:ease-in duration-150 focus:ring-2 focus:ring-slate-900' />
+                
+            </div>
         </div>
-
-        <button className='bg-red-600 px-3 py-1 rounded' 
-            onClick={()=>{props.getModal()}}
-            >
-            cancel
-        </button>
+        
+        <div className='w-full mt-3 flex justify-end gap-5'>
+            <button className='button-54 bg-red-600' 
+                onClick={()=>{props.getModal()}}
+                >
+                cancel
+            </button>
+            <button className='button-54 bg-violet-400' onClick={handleSave}>
+                Save
+            </button>
+        </div>
     </div>
   )
 }
