b4a2ee794b6ff1456e59732ad7636999e5bcdc7e
Hamdan Salih <hamdansalihpc@gmail.com>
2023-12-06 08:52:36 +0530

    new login

--- src/pages/account/Login.jsx
+++ src/pages/account/Login.jsx
@@ -3,24 +3,31 @@ import api from '../../api/api';
 import { useState } from 'react';
 
 import './login.css'
-import { useSelector } from 'react-redux';
+import { addUserDetails } from '../../redux/slices/userSlice';
+import { addTokens } from '../../redux/slices/tokenSlice';
+import {  useDispatch } from 'react-redux';
+import { jwtDecode } from "jwt-decode";
+// import { Navigate } from 'react-router-dom';
 
 
 
 const Login = () =>{
 
     const [Data, setData] = useState({email:"" , password : ""});
-    
-    const testState = useSelector(state => state.user)
-   
+    const dispatch = useDispatch()
+
     const handlleSubmit = () => {
         console.log(Data);
-        api.post('/account/login/', Data)
+        api.post('/account/token/', Data)
         .then(function (response) {
-            console.log(response.data.user);
-            console.log("testing");
+            if(response.status == 200){
+                console.log(response.data)
+                dispatch(addTokens(response.data))
+                dispatch(addUserDetails(jwtDecode(response.data.access)))
+            }
         })
         .catch(function (error) {
+            alert("Invalid Credential")
             console.log(error);
         });
         setData({email:"" , password : ""})
@@ -31,10 +38,26 @@ const Login = () =>{
     
     return (
         <>
+        {console.log()}
             <div className='grid gap-6 pt-5'>
                 {/* {console.log(testState)} */}
-                <input onChange={handleChange} value={Data.email} type="text" name='email' placeholder='Email address' className='border border-slate-500 px-4 py-3 w-96'/>
-                <input onChange={handleChange} value={Data.password} type="password" name='password' placeholder='Password'  className='border border-slate-500 px-4 py-3 w-96'/>
+                <input onChange={handleChange} 
+                    id='email' 
+                    value={Data.email} 
+                    type="text" 
+                    name='email' 
+                    placeholder='Email address' 
+                    className='border border-slate-500 px-4 py-3 w-96' 
+                    onKeyDown={(e)=> {if(e.key === "Enter"){document.getElementById('password').focus()}}}/>
+                <input onChange={handleChange} 
+                    id='password' 
+                    value={Data.password} 
+                    type="password" 
+                    name='password' 
+                    placeholder='Password'  
+                    className='border border-slate-500 px-4 py-3 w-96'
+                    onKeyDown={(e)=>{if(e.key === "Enter" && Data.password!==""){handlleSubmit()}}}    
+                    />
             </div>
             <div className='flex gap-6 pt-5'>
                 <button className='login-btn px-5 py-3' onClick={handlleSubmit}>Login</button>
