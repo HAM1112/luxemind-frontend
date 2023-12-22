b3edf9d6ed2e97e4ff29ee5e51276d4ff46b4728
Hamdan Salih <hamdansalihpc@gmail.com>
2023-12-22 21:42:15 +0530

    checkauth updated

--- src/utilities/CheckAuth.jsx
+++ src/utilities/CheckAuth.jsx
@@ -5,12 +5,34 @@ import { Navigate } from "react-router-dom";
 
 function CheckAuth(props){
     const user = useSelector(state => state.user.value)
+    let currentDate = new Date();
     
-
+    
     if (user !== null) {
+        if (user.exp * 1000 < currentDate.getTime()) {
+            
+            return
+        }
+        console.log('not expired');
         if (user.is_superuser) {
+            if(props.from === "admin"){
+                return props.children
+            }
             return <Navigate to={'/admin'} />
         }
+        if(user.is_provider){
+            if(props.from === "provider"){
+                return props.children
+            }
+            return <Navigate to={"/provider"} />
+        }
+        if(!user.is_provider){
+            if(props.from === "student"){
+                return props.children
+            }
+            return <Navigate to={"/student"} />
+        }
+        
     } else{
         console.log(user)
         if (props.role === "login") {
