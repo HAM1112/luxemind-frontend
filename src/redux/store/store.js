f8cda6d459a1dae3b9465b0d9b0573d6fed07748
Hamdan Salih <hamdansalihpc@gmail.com>
2023-12-23 21:43:24 +0530

    create redux store

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,9 +1,11 @@
 
 import { configureStore } from '@reduxjs/toolkit'
 import userReducer from '../slices/userSlice'
+import tokenReducer from '../slices/tokenSlice'
 
 export default configureStore({
   reducer: {
     user : userReducer,
+    token : tokenReducer,
   },
 })
