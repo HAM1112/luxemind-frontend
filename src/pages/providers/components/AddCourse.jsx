31a64ffaf1255b62c01604fe1be8c8e73362c345
Hamdan Salih <hamdansalihpc@gmail.com>
2023-12-25 21:53:48 +0530

    add course and lessonn

---
+++ src/pages/providers/components/AddCourse.jsx
@@ -0,0 +1,273 @@
+import React, { useEffect, useState } from 'react'
+import './addcourse.css'
+import { TiTick } from "react-icons/ti";
+import Detalis from './addcoure/Detalis';
+import Files from './addcoure/Files';
+import Additional from './addcoure/Additional';
+import api from '../../../api/api';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import useUrlHeader from '../../../utilities/urlHeader';
+import { useSelector } from 'react-redux';
+
+function AddCourse(props) {
+
+  const steps = ["Details", "Files", "Extra"];
+  const [currentStep, setCurrentStep] = useState(1);
+  const [complete, setComplete] = useState(false);
+  const auth = useUrlHeader()
+  const [course, setCourse] = useState({
+    name : '',
+    description : '' ,
+    about : '',
+    short_description : '',
+
+    course_thumbnail : '',
+    course_preview : '',
+    certificate_url : '',
+
+    subject : '',
+    course_price : 0,
+    no_of_days : 0,
+    level : '',
+    prerequisites : '',
+  });
+
+  useEffect(() => {
+    setCourse(props.course)
+    if(props.course){
+      console.log(props.course);
+      api.get(
+        `provider/getCourseDetails/${props.course.id}`,
+        auth
+      ).then((response) =>{
+        setCourse(response.data)
+      })
+    }  
+  }, []);
+  
+  useEffect(() => {
+    console.log('re rendering');
+  }, [course]);
+  
+  
+  const getData = (data) => {
+    setCourse(data)
+  }
+  let components= [<Detalis course={course} onChange={getData}/> , <Files course={course} onChange={getData}/> , <Additional course={course} onChange={getData}/> ]
+  
+  
+  const handleNext = () => {
+
+    if (currentStep === 1 ){
+        if (course.name == '') {
+          toast.error('Course name not added', {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            });
+          return      
+        }
+        if(course.short_description == ""){
+          toast.error('Short description not added', {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            });
+          return      
+        }
+        if (course.description == '') {
+          toast.error('Description not added', {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            });
+          return      
+        }
+        if (course.about == '') {
+          toast.error('About not added', {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            });
+          return      
+        }
+      }
+      //now here
+      if (currentStep === 2) {
+        if(course.course_thumbnail == ""){
+          toast.error('Thumnail not added', {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            });
+          return      
+      }
+        if(course.course_preview == ""){
+          toast.error('Preview not added', {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            });
+          return      
+      }
+        if(course.certificate_url == ""){
+          
+          toast.error('Certificate not added', {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            });
+          return      
+      }
+    }
+
+
+    currentStep === steps.length
+              ? setComplete(true)
+              : setCurrentStep((prev) => prev + 1);
+  }
+  
+  const handlePrev = () => {
+    setComplete(false)
+    if (currentStep !== 1) {
+      setCurrentStep((prev)  => prev - 1 );
+    }
+  }
+
+  const handleSave = () => {
+    console.log(props.edit)
+    if (props.edit === "true") {
+      console.log(course);
+      api.put(
+        `provider/updateCourse/${course.id}`,
+        course,
+        auth
+      ).then((response)=>{
+        console.log(response);
+        toast.success('nice' , {
+          position: "top-center",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+          });
+          props.getModal(false)
+      }).catch((error)=>{
+        console.log('this is the first error in course upload btn clicked');
+      })
+      
+    } else {
+      api.post(
+        'provider/addCourse/',
+        course,
+        auth
+        ).then((response)=>{
+          console.log(response);
+          toast.success('nice' , {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+          });
+          props.getModal(false)
+        }).catch((response)=>{
+          console.log('this is the secound error in course upload btn clicked');
+          const message = response.response.data.short_description['0']
+          console.log(Object.keys(response.response.data)[0]);
+          toast.error(message, {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            });
+        })
+      }
+    }
+    
+    
+    return (
+      <div className='add-course px-4 bg-white rounded overflow-x-scroll relative border-2 border-red-700 '>
+      <ToastContainer />
+      <div className='mb-4'>
+          <div className="sticky py-8 top-0 flex justify-between w-full bg-white">
+            {steps?.map((step, i) => (
+              <div
+                key={i}
+                className={`step-item ${currentStep === i + 1 && "active"} ${
+                  (i + 1 < currentStep || complete) && "complete"
+                } `}
+              >
+                <div className="step">
+                  {i + 1 < currentStep || complete ? <TiTick size={24} /> : i + 1}
+                </div>
+                <p className="text-gray-500">{step}</p>
+              </div>
+            ))}
+          </div>
+          <div className='pt-4'>
+            {components[currentStep - 1 ]}
+          </div>
+      </div>
+      <div className='sticky pt-4 left-0 bottom-0 flex justify-end gap-5 bg-white w-full overflow-y-hidden p-2'>
+          {currentStep === 1 ?
+            <button className='px-3 rounded bg-slate-400' disabled>Prev</button>
+            : <button className='px-3 rounded bg-violet-400' onClick={handlePrev}>Prev</button>
+          }
+          {!complete && (
+            currentStep === steps.length ? <button className='px-3 rounded bg-violet-700' onClick={handleSave}>Save</button> 
+            :  <button className='px-3 rounded bg-violet-400' onClick={handleNext}>Next</button>
+          )}
+        <button className='px-2 py-3 bg-red-700 rounded' onClick={()=>{props.getModal(false);console.log('clicked')}}>Cancel</button>
+      </div>
+    </div>
+  )
+}
+
+export default AddCourse
---
+++ src/pages/providers/components/AddLesson.jsx
@@ -0,0 +1,121 @@
+import React, { useEffect, useState } from 'react'
+import { Player } from 'video-react';
+import { convertToUnderscore } from '../../../utilities/convertToUnderscore';
+import { firebasestore } from '../../../firebase/firebase';
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage'
+import 'video-react/dist/video-react.css';
+import api from '../../../api/api';
+import useUrlHeader from '../../../utilities/urlHeader';
+import { SpinnerCircular } from 'spinners-react';
+
+function AddLesson(props) {
+
+    const {getModal , module_id} = props
+    const [file , setFile] = useState(null)
+    const auth = useUrlHeader()
+    const [loading, setLoading] = useState(false);
+    const [lesson, setLesson] = useState({
+        name : '',
+        lesson_url : '',
+        module : module_id,
+    });
+    const handleFileInput = (e) => {
+        if (lesson.name == '') {
+            alert('Add name first')
+            e.target.value = '',
+            setFile(null)
+            return
+        }
+        setFile(e.target.files[0])
+    }
+
+    const handleChange = (e) => {
+        setLesson({...lesson, [e.target.name] : e.target.value})
+    }
+
+    const handleSubmit = () => {
+        if (lesson.name === '' || lesson.lesson_url === '') {
+            alert('name or video is not added')
+            return
+        }
+        setLoading(true)
+        api.post(
+            '/provider/addLesson/',
+            lesson,
+            auth
+        ).then((response)=>{
+            if (response.status === 201) {
+                console.log('Lesson added Successfully');
+            }else{
+                console.log(response.data);
+            }
+            getModal(false)
+        }).catch((erro)=>{
+            console.log(erro)
+        })
+        setLoading(false)
+    }
+    
+    useEffect(() => {
+        
+    }, [loading]);
+    
+    useEffect(() => {
+        if(file == null){
+            console.log('file is null')
+            return
+        }
+        setLoading(true)
+        console.log(file);
+        const lesson_name = convertToUnderscore(lesson.name)
+        const fileRef = ref(firebasestore , `courses/lessons/${module_id}_${lesson_name}`)
+        uploadBytes(fileRef , file)
+        .then((snapshot) => {
+            getDownloadURL(snapshot.ref).then(url => {
+                console.log("get url worked");
+                setLesson(prev => ({...prev, lesson_url : url}))
+                console.log('image uploaded successfully');
+            }).catch(error => {
+                console.log('error in getdownload url');
+            })
+        }).catch(error => {
+            console.log('error in uploadbytes');
+        })
+        setLoading(false)
+    }, [file]);
+
+  return (
+    <div className='w-[800px] p-14 bg-white rounded'>
+        <div>
+            <div className='grid gap-1 mt-2'>
+            <h3 className='font-semibold'>Name</h3>
+            <input onChange={handleChange} name='name' type="text" className='border-2 border-slate-900 p-1 rounded'/>
+            </div>
+        </div>
+
+        <div className='grid gap-1 border-b-2 pb-2'>   
+          <h3 className='font-semibold my-2'>Add preview video</h3>
+          <div className='flex items-center gap-1 w-ful'>
+            <div className='w-7/12'>
+              <input name='lesson_video' onChange={handleFileInput} accept='video/*' className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" type="file" />
+            </div>
+            <div className='file-display w-5/12 flex items-center text-center'>
+                { lesson.lesson_url ? <Player>
+                    <source src={ lesson.lesson_url } />
+                  </Player> : <p className='w-full h-full'>No preview</p>}
+            </div>     
+          </div>
+        </div>
+
+      <div className='flex justify-end gap-5 my-6'>
+        {/* {loading ?  } */}
+        <button className='px-3 rounded text-white bg-violet-500' onClick={handleSubmit}>
+            { loading ? <SpinnerCircular size={50} thickness={100} speed={100} color="rgba(255, 255, 255, 1)" secondaryColor="rgba(255, 255, 255, 1)" /> : <>Save</> }
+        </button>
+        <button className='px-3 py-2 text-white bg-red-700 rounded' onClick={()=>getModal(false)}>Cancel</button>
+      </div>
+    </div>
+  )
+}
+
+export default AddLesson
