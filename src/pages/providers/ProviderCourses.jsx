db60fa245f2041ee362bc4749f712d06d172d411
Hamdan Salih <hamdansalihpc@gmail.com>
2023-12-26 15:58:47 +0530

    providercourse and dashboard update

--- src/pages/providers/ProviderCourses.jsx
+++ src/pages/providers/ProviderCourses.jsx
@@ -1,9 +1,95 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import CourseCard from '../partials/CourseCard'
+import api from '../../api/api';
+import useUrlHeader from '../../utilities/urlHeader';
+import { useSelector } from 'react-redux';
+import { convertToUnderscore } from '../../utilities/convertToUnderscore';
+
 
 function ProviderCourses() {
+
+  const [modal, setModal] = useState(false);
+  const [courses, setCourses] = useState([]);
+  const [subjects, setSubjects] = useState([]);
+
+  
+  const auth = useUrlHeader()
+  useEffect(() => {
+    api.get(
+      '/provider/list-subjects/',
+      auth
+    ).then(response => {
+      setSubjects(response.data)
+    }).catch(error => {
+      console.log(error.code);
+    })
+  }, []);
+  useEffect(() => {
+    api.get(
+      '/provider/list-courses/',
+      auth
+    ).then((response) => {
+      setCourses(response.data)
+      console.log(response.data);
+    }).catch((error)=>{
+      console.log('newtwork issue');
+    })
+
+  }, []);
+
+  const getModal = (bool) => {
+      setModal(bool)
+  }
+
   return (
-    <div>
-      this is the page for courses
+    <div className='border-2 border-yellow-300 w-full p-10 bg-[linear-gradient(225deg, #f0f0f0, #cacaca);]'>
+      <div>
+        <h1 className='font-bold'>Unpublished Courses</h1>
+        <div className='mt-5 grid grid-cols-1 justify-items-stretch sm:grid-cols-2  lg:grid-cols-3 xl:grid-cols-4 gap-5'>
+          {
+            courses.filter(course => !course.is_published && !course.is_pending ).map((course) => {
+               
+              let selectedSubject = subjects.find(item => item.id === course.subject);
+              
+              return (
+                <div><CourseCard details={course} subject={selectedSubject.name}/></div>
+              )
+            })
+          }
+        </div>
+      </div>
+
+      <div className='mt-10'>
+        <h1 className='font-bold'>Pending Approval</h1>
+        <div className='mt-5 grid grid-cols-1 justify-items-stretch sm:grid-cols-2  lg:grid-cols-3 xl:grid-cols-4 gap-5'>
+          {
+            courses.filter(course => !course.is_published && course.is_pending ).map((course) => {
+               
+              let selectedSubject = subjects.find(item => item.id === course.subject);
+              
+              return (
+                <div><CourseCard details={course} subject={selectedSubject.name}/></div>
+              )
+            })
+          }
+        </div>
+      </div>
+
+      <div className='mt-10'>
+        <h1 className='font-bold'>Published Courses</h1>
+        <div className='mt-5 grid grid-cols-1 justify-items-stretch sm:grid-cols-2  lg:grid-cols-3 xl:grid-cols-4 gap-5'>
+          {
+            courses.filter(course => course.is_published && !course.is_pending ).map((course) => {
+               
+              let selectedSubject = subjects.find(item => item.id === course.subject);
+              
+              return (
+                <div><CourseCard details={course} subject={selectedSubject.name}/></div>
+              )
+            })
+          }
+        </div>
+      </div>
     </div>
   )
 }
--- src/pages/providers/ProviderDashboard.jsx
+++ src/pages/providers/ProviderDashboard.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import picExample from './../../assets/idea.png'
 import gradpic from './../../assets/graduation.png'
 import './providerhome.css'
+import CustomModal from '../customs/CustomModal';
+import AddCourse from './components/AddCourse';
+import api from '../../api/api';
+import useUrlHeader from '../../utilities/urlHeader';
+import { useNavigate } from 'react-router-dom';
 
 function ProviderDashboard() {
+
+    const [modal, setModal] = useState(false);
+    const [user, setUser] = useState({});
+    const auth = useUrlHeader()
+    const navigate = useNavigate()
+    useEffect(() => {
+        api.get(
+            '/provider/profile',
+            auth
+        ).then((response) => {
+            setUser(response.data)
+            
+        })
+    }, []);
+
+
+    useEffect(() => {
+        if (
+          user.dob === null ||
+          user.education === '' ||
+          user.first_name === '' ||
+          user.last_name === '' ||
+          user.about === ''
+        ){
+          console.log('not ddd' || user.education == '' || user.first_name == "" || user.last_name == '')
+          navigate('/provider/profile')
+        }
+      }, [user]);
+
+
+    const getModal = (bool) => {
+        setModal(bool)
+    }
+
+
   return (
     <div className='px-32 py-28 w-full'>
       <div className='flex  w-full '>
@@ -13,12 +53,20 @@ function ProviderDashboard() {
                 <img className='add-course-image w-56' src={picExample} alt="" />
             </div>
             <div className='text-center'>
-                <button className='mt-12 bg-red-500 px-3 py-1 text-white'>
+                <button 
+                className='mt-12 bg-red-500 px-3 py-1 text-white'
+                onClick={() => {
+                    console.log('add course clicked')
+                    setModal(true)
+                }}
+                >
                     Add Course
                 </button>
             </div>
         </div>
+        
 
+        {modal && <CustomModal component={<AddCourse getModal={getModal}  />} />}
 
         <div className='flex-2 w-2/5 bg-gray-200 p-6 rounded'>
             <h2 className='font-bold'>Unpublished</h2>
