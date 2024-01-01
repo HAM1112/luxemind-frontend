3cb73782dc0c3621a15bf630a2a4a0edf1cf6cfe
Hamdan Salih <hamdansalihpc@gmail.com>
2024-01-01 22:31:01 +0530

    update lesson page

--- src/pages/partials/FullCourse.jsx
+++ src/pages/partials/FullCourse.jsx
@@ -5,16 +5,18 @@ import ReactPlayer from 'react-player'
 import { Link, Outlet, useParams } from 'react-router-dom'
 import api from '../../api/api'
 import useUrlHeader from '../../utilities/urlHeader'
+import { useSelector } from 'react-redux'
 
 
 
 function FullCourse() {
   const {course_id} = useParams()
-  
-  const [courseDetails, setCourseDetails] = useState({});
+  const user = useSelector(state => state.user.value)
+  const [courseDetails, setCourseDetails] = useState(null);
   const [curriculum, setCurriculum] = useState([]);
+  const [p_lessons, setP_lessons] = useState([]);
   const auth = useUrlHeader()
-
+  const [loading, setLoading] = useState(false);
   const toggleModule = (index) => {
     const updatedAll = [...curriculum];
     updatedAll[index].is_open = !updatedAll[index].is_open;
@@ -22,6 +24,7 @@ function FullCourse() {
   };
 
   useEffect(() => {
+    setLoading(true)
     api.get(`/provider/getCurriculumDetails/${course_id}` , auth)
     .then(response => {
       const updatedData = response.data.map((module) => ({...module , is_open : false}))
@@ -32,45 +35,98 @@ function FullCourse() {
       auth
     ).then(response => {
       setCourseDetails(response.data)
-      console.log(response.data);
     }).catch(error => console.log('error from FullCourse courseDetails get'))
+    if (!user.is_provider && !user.is_superuser) {
+      api.get(
+        `/student/getCourseDetails/${course_id}`,
+        auth
+      ).then(response=>{
+        setP_lessons(response.data)
+      })  
+    } 
+    setLoading(false)
   }, []);
+
+  const getCompletePercentage = ()=>{
+    let total = "00:00"
+    curriculum.map(module => {[
+      module.lessons.map(lesson => {
+        const [lessonMinutes, lessonSeconds] = lesson.lesson_duration.split(":");
+        total = addTimeToTotal(total, parseInt(lessonMinutes), parseInt(lessonSeconds));
+      })
+    ]})
+    
+    let completed = "00:00"
+    curriculum.map(module => {[
+      module.lessons.map(lesson => {
+        const p_lesson = findLessonById(lesson.id)
+        if (p_lesson && p_lesson.is_compelete) {
+          const [lessonMinutesp, lessonSecondsp] = lesson.lesson_duration.split(":");
+          completed = addTimeToTotal(completed, parseInt(lessonMinutesp), parseInt(lessonSecondsp));
+        }
+      })
+    ]})
+    
+    
+    
+    return parseInt((convertTimeToSeconds(completed)/convertTimeToSeconds(total))*100)
+
+  }
+  const addTimeToTotal = (total, minutes, seconds) => {
+    const [totalMinutes, totalSeconds] = total.split(":");
+    const newTotalSeconds = parseInt(totalMinutes) * 60 + parseInt(totalSeconds) + minutes * 60 + seconds;
+    // Convert total seconds back to "mm:ss" format
+    return `${String(Math.floor(newTotalSeconds / 60)).padStart(2, '0')}:${String(newTotalSeconds % 60).padStart(2, '0')}`;
+  };
+
+  function convertTimeToSeconds(timeString) {
+    // Split the time string into minutes and seconds
+    const [minutes, seconds] = timeString.split(':').map(Number);
+
+    // Calculate the total seconds
+    const totalSeconds = minutes * 60 + seconds;
+
+    return totalSeconds;
+}
 
   useEffect(() => {
     
-  }, [courseDetails , curriculum]);
+  }, [loading]);
+
+  const findLessonById = (id) => p_lessons.find(lesson => lesson.lesson === id);
 
   return (
+    <>
+    {!loading ?
+    
     <div className='flex h-screen'>
-
+      
       {/* video side */}
-      <div className='w-2/3  p-10'>
+      <div className={`${user.is_provider ? 'w-2/3' : 'w-3/4' }  py-3 px-24`}> 
         <Outlet />
       </div>
 
 
 
       {/* Curriculum full details */}
-      <div className='w-1/3 py-8 py-3 bg-[#eaeaea] overflow-y-scroll border-x border-gray-400'>
-
+      <div className={`${user.is_provider ? 'w-1/3' : 'w-1/4' } py-8 py-3 bg-[#eaeaea] overflow-y-scroll border-x border-gray-400`}>
         <div className='p-5'>
-          <h2 className='text-[24px] font-semibold'>{courseDetails.course.name}</h2>
-          <div className="w-full bg-gray-300 rounded-full h-2.5 dark:bg-gray-700 my-5">
-            <div className="bg-[#ceaf95] h-2.5 rounded-full" style={{width : `${89}%`}}></div>
-          </div>  
+          <h2 className='text-[24px] font-semibold'>{courseDetails ? courseDetails.course.name : null}</h2>
+          <div className='flex items-center gap-2'>
+            <div className="w-full bg-gray-300 rounded-full h-2.5 dark:bg-gray-700 my-5">
+              <div className="bg-[#ceaf95] h-2.5 rounded-full" style={{width : `${getCompletePercentage()}%`}}></div>
+            </div>
+            <p className='pb-1'>{getCompletePercentage()}%</p>
+          </div>
         </div>
-         
          {/* contents start here */}
-
-
         {curriculum.map((module , index) => {
-          
           return (
             <div key={index}>
                   <div className='flex bg-[#DBDBDB] justify-between py-3  px-5'>
                     <p>{module.module.name}</p>
-                    <div className='flex gap-5'>
-                      <span>0/{module.lessons.length}</span>
+                    <div className='flex gap-5 items-start'>
+                      {/* <span>0/{module.lessons.length}</span> */}
                       <span onClick={()=>{toggleModule(index)}} className='bg-[#eaeaea] rounded-full px-1'><FontAwesomeIcon icon={faChevronDown} /></span>
                     </div>
                   </div>
@@ -79,13 +135,19 @@ function FullCourse() {
                   <div className='w-full border-b border-gray-500 px-8 '>
                     <ul>
                       {module.lessons.map((lesson,index) => {
+                        const lessontrack = findLessonById(lesson.id)
                         return (
                           <li key={index} className='px-3 py-1 flex justify-between border-b border-gray-400'>
                             <div>
                               <h1 className='text-[14px] capitalize'><Link to={`lesson/${lesson.id}`}>{lesson.name}</Link></h1>
-                              <div><FontAwesomeIcon className='text-red-400 text-[14px]' icon={faCirclePlay} /> <span className='ps-1 text-gray-600 text-[14px]'>10:00</span></div>
+                              <div><FontAwesomeIcon className='text-red-400 text-[14px]' icon={faCirclePlay} /> <span className='ps-1 text-gray-600 text-[14px]'>{lesson.lesson_duration}</span></div>
                             </div>
-                            <span><FontAwesomeIcon className='text-[#b79c84]' icon={faCircleCheck} /></span>
+                            { 
+                              lessontrack.is_compelete ?
+                              <span><FontAwesomeIcon className='text-[#b79c84]' icon={faCircleCheck} /></span>
+                              : 
+                              <span><FontAwesomeIcon className='text-[#B7B7B7]' icon={faCircleCheck} /></span>
+                             }
                           </li>
                         )
                       })}
@@ -93,14 +155,14 @@ function FullCourse() {
                   </div>
                   }
             </div>
-            
           )
         })}
         {/* content ends here */}
-
       </div>
-
     </div>
+    :null
+    }
+    </>
   )
 }
 
--- src/pages/partials/Lesson.jsx
+++ src/pages/partials/Lesson.jsx
@@ -10,7 +10,9 @@ import { SpinnerCircular } from 'spinners-react';
 
 function Lesson() {
     const [lesson, setLesson] = useState(null);
+    const [p_lessons, setP_lessons] = useState(null);
     const [lessonsArray, setLessonsArray] = useState([]);
+    const [purchaseLesson, setPurchaseLesson] = useState({});
     const {course_id ,lesson_id} = useParams()
     const [loading, setLoading] = useState(false);
     const user = useSelector(state => state.user.value)
@@ -30,7 +32,14 @@ function Lesson() {
             setLessonsArray(items)
         })
         .catch(error => {console.log('error in Lesson getcurricculum api')})
-       
+        if (!user.is_provider && !user.is_superuser) {
+          api.get(
+            `/student/getCourseDetails/${course_id}`,
+            auth
+          ).then(response=>{
+            setP_lessons(response.data)
+          })  
+        } 
         const timeoutId = setTimeout(() => {
           setLoading(false);
         }, 2000);
@@ -38,49 +47,126 @@ function Lesson() {
     }, [lesson_id]);
 
     useEffect(() => {
-      
+   
     }, [lesson , lessonsArray ]);
+    
+    useEffect(() => {
+      // current index from lessonArray
+      const current = lessonsArray.findIndex(lessonn => lessonn.id == lesson_id)
+      if (current > 0 && p_lessons != null && lessonsArray && p_lessons) {
+        
+        const prev_id = lessonsArray[current - 1].id
+        const prevFromPurchase = p_lessons.find(less => less.lesson === prev_id )
+        console.log(prev_id);
+        
+
+        if ( current > 0 && prevFromPurchase.is_watched === false ) {
+          navigate(`/student/course/${course_id}/lesson/${lessonsArray[ current - 1 ].id}`)
+        }
+
+        setPurchaseLesson(p_lessons.find(pl => pl.lesson == lesson_id))
+      }
+    }, [p_lessons]);
 
 
     const handlePrev = () => {
         const index = lessonsArray.findIndex(lesson => lesson.id == lesson_id)
-        if(index >= 1){
-            // console.log(lessonsArray[index-1].id)
-            navigate(`/provider/course/${course_id}/lesson/${lessonsArray[ index - 1 ].id}`)
+        if(index >= 1 && lessonsArray ){
+            if (user.is_provider) {
+              navigate(`/provider/course/${course_id}/lesson/${lessonsArray[ index - 1 ].id}`)
+            }else if (user.is_superuser) {
+              console.log('Admin Side Working');
+            }else{
+              navigate(`/student/course/${course_id}/lesson/${lessonsArray[ index - 1 ].id}`)
+            }
+            console.log('prev working');
         }
     }
     const handleNext = () => {
         const index = lessonsArray.findIndex(lesson => lesson.id == lesson_id)
-        if (index < lessonsArray.length-1){
+        if (index < lessonsArray.length-1 && lessonsArray ){
+          if (user.is_provider) {
             navigate(`/provider/course/${course_id}/lesson/${lessonsArray[ index + 1 ].id}`)
+          }else if (user.is_superuser) {
+            console.log('Admin Side working');
+          }else{
+            const current = p_lessons.find(p_l => p_l.lesson == lesson_id)
+            
+            if (current.is_watched) {
+              navigate(`/student/course/${course_id}/lesson/${lessonsArray[ index + 1 ].id}`)
+            }
+          }
+          console.log('next working');
         }
+    }
+
+    const handleComplete = () =>{
+      if (!user.is_provider && !user.is_superuser) {
+        const completeLesson = p_lessons.find(plesson => plesson.lesson == lesson_id)
+        api.patch(
+          `/student/update_complete/${completeLesson.id}`,
+          null,
+          auth
+        ).then(response => {
+          console.log(response.data);
+          completeLesson.is_compelete = !completeLesson.is_compelete
+          setP_lessons([completeLesson,...p_lessons])
+        }).catch(error=>{
+          console.log('erro in complete toggle');
+        })
+      }
+    }
+
+    const handleWatch = () => {
+      if (!user.is_provider && !user.is_superuser) {   
+        const watchedLesson = p_lessons.find(plesson => plesson.lesson == lesson_id)
+        api.patch(
+          `/student/update_watched/${watchedLesson.id}`,
+          null,
+          auth
+        ).then(response =>{
+          console.log(response.data);
+          watchedLesson.is_watched = true
+          setP_lessons([watchedLesson , ...p_lessons])
+        }).catch(error => {
+          console.log('errro in watched updated');
+        })
+      }
+
     }
 
   return (
     <>  
       { lesson ? <>
+        
         <h2 className='text-[20px] text-gray-600 py-3'>{lesson.module.name}</h2>
         <h1 className='text-[30px] text-center font-bold py-3'>{lesson.lesson.name}</h1>
 
-        <div className='flex justify-center my-8 border-4 border-red-700'>
+        <div className='flex justify-center my-8 '>
           <ReactPlayer 
             url={lesson.lesson.lesson_url}
             controls={true}
             volume={1}
             muted={false}
-            onEnded={()=>console.log('this is it')}
+            onEnded={handleWatch}
             />
         </div>
 
-        <div className='border-2 border-violet-600 select-none flex justify-evenly w-full py-3'>
+        <div className='select-none flex justify-evenly w-full py-3'>
           <div onClick={handlePrev} className='flex gap-3 border-2 border-gray-300 py-4 px-6 rounded-xl hover:bg-gray-100'>   
             <span className='rounded-full px-1 border-2 border-black'><FontAwesomeIcon icon={faArrowLeft} /></span>
             <p>Previous Lesson</p>
           </div>
-          {user.is_provider ? null :
-            <div className='flex gap-3 text-white select-none border-2 justify-center py-4 px-6 rounded-xl w-[300px] bg-[#2E4374]'>
-                Mark Complete
-            </div>
+          {user.is_provider || user.is_superuser ? null :
+            
+              
+              <div onClick={handleComplete}  className='flex gap-3 text-white select-none border-2 justify-center py-4 px-6 rounded-xl w-[300px] bg-[#2E4374]'>
+                  {
+                    purchaseLesson.is_compelete ? 
+                    <>Remove Complete</> : <>Mark Complete</>
+                  }
+              </div>
+             
           }
           <div onClick={handleNext} className='flex gap-3 border-2 border-gray-300 select-none py-4 px-6 rounded-xl w-[180px] hover:bg-gray-100'>   
             <p>Next Lesson</p>
