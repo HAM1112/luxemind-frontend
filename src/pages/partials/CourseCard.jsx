3606530e22f4e6415ee7ca9a3b2733ce46326937
Hamdan Salih <hamdansalihpc@gmail.com>
2024-01-11 15:52:30 +0530

    course details updated

--- src/pages/partials/CourseCard.jsx
+++ src/pages/partials/CourseCard.jsx
@@ -7,125 +7,166 @@ import { useNavigate } from 'react-router-dom';
 import api from '../../api/api';
 import useUrlHeader from '../../utilities/urlHeader';
 import { useSelector } from 'react-redux';
+import time from '../../assets/time.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 
-function CourseCard({details , subject , is_admin , provider}) {
+function CourseCard({details , subject , is_admin , provider , purchase}) {
     const [course, setCourse] = useState(details);
     const [modal, setModal] = useState(false);
+    const [is_favorite, setIs_favorite] = useState(false);
     const sub = subject
     const auth = useUrlHeader()
     const navigate = useNavigate()
     const user = useSelector(state => state.user.value)
+    const [purchaseDetails, setPurchaseDetails] = useState(purchase)
     const getModal = (bool) => {
         setModal(bool)
     }
     useEffect(() => {
-        
-    }, [course]);
+        api.get(
+            `/student/checkwishlist/${course.id}`,
+            auth
+        ).then(response => {
+            setIs_favorite(response.data.is_favorite)
+        }).catch(error =>{
+            console.log(error);
+        })
+    }, []);
+    useEffect(() => {
+    }, [course , is_favorite]);
     const handlePublish = (e)=>{
         const id = course.id
         api.put(
             `/provider/publish_course/${id}`,
             {course_id : id},
             auth
-        ).then(response => {
-            setCourse(response.data)
+            ).then(response => {
+                setCourse(response.data)
         }).catch(error =>{
             console.log(error);
         })
     }
     const handleApprove = ()=>{
         const id = course.id
-        api.put(
-            `/adminpanel/approve_course/${id}`,
-            {course : id},
-            auth
-        )
+        api.put(`/adminpanel/approve_course/${id}`,{course : id})
+        const test = course 
+        test.is_published = true
+        setCourse(test)
     }
-
-
-  return (
-    <>
-
-        <div className='course-card h-[320px] relative rounded-2xl select-none '>
+    const handleBlock = () => {
+        const id = course.id
+        api.put(`/adminpanel/block_course/${id}`,{course : id})
+    }
+    function daysLeftUntil(end_date) {
+        const endDate = new Date(end_date);
+        const currentDate = new Date();
+        const differenceInMs = endDate - currentDate;
+        const daysLeft = Math.floor(differenceInMs / (1000 * 60 * 60 * 24));  
+        return daysLeft;
+    }
+    const getDate = (date) => {
+        const date_to_extrack = new Date(date);
+        const formattedDate = date_to_extrack.toISOString().split('T')[0];
+        return formattedDate
+    }
+    const toggleWishList = () =>{
+        api.post(
+            `/student/wishlist/` ,
+            {course_id : course.id},
+            auth
+        ).then(response => {
+            setIs_favorite(response.data.fav)
+        }).catch(error => {
+            console.log(error);
+        })
+    }
+        return (
+            <>
             <div onClick={() => { 
                 console.log('course open')
+                if(subject === 'enrolled'){
+                    return navigate(`/student/courses/${course.id}`)
+                }
                 navigate(`${course.id}`)
             }}>
-
-            
-                <div className=' w-full h-[150px] rounded-[20px]'>
-                    <img className='h-full w-full rounded-[20px]' src={course.course_thumbnail} alt="" />
+                <div className=' w-full h-[150px] '>
+                    <img className='h-full w-full z-[-1] overflow-hidden' src={course.course_thumbnail} alt="" />
                 </div>
                 <div className='content absolute w-full h-[195px]  bottom-0 '>
-                    <div className='bg-white p-6 h-full rounded-b-2xl'>
+                    <div className='bg-white px-4 py-2 h-full '>
                         <h1 className='text-[18px] font-semibold capitalize truncate' title={course.name}>{course.name}</h1>
                         {
                             is_admin ? <>
-
                             {/* <p className='text-gray-500 font-semibold mt-3 capitalize'>{provider}</p> */}
                             {null}
                             </>
                             :
-                            <p className='text-gray-500 font-semibold mt-3 capitalize'>{subject}</p>
-
+                            <p className='text-gray-500 font-semibold mt-3 capitalize'>{subject === 'enrolled' ? null : subject}</p>
+                        }
+                        {
+                            purchaseDetails ? 
+                            <div className=''>
+                                {
+                                    !purchaseDetails.is_paid &&
+                                    <p>{(daysLeftUntil(purchaseDetails.end_date)) < 0 ? <>Course Expired</> : <>{daysLeftUntil(purchaseDetails.end_date)} Days Left</>}</p>
+                                }
+                                <div className='flex justify-between items-center mt-2'>
+                                    <div className='flex items-center gap-2'>
+                                        <img src={time} alt="" className='w-4 h-4'/>
+                                        <p className='text-xs'>{purchaseDetails.total_length}</p>
+                                    </div>
+                                    <p className='text-xs'>{purchaseDetails.complete_percentage}%</p>
+                                </div>
+                                <div className="w-full bg-[#e0e0e0] h-2 dark:bg-gray-700 my-5">
+                                    <div className="bg-[#ceaf95] h-full" style={{width : `${purchaseDetails.complete_percentage}%`}}></div>
+                                </div>
+                            </div>
+                            :
+                            null
                         }
                     </div>
                 </div>
             </div>
-            {/* published of not just above are same */}
-
                     {
-                        !user.is_superuser && !user.is_provider ?  
-                        <div className='absolute b-[20px] w-full flex px-5 gap-4 bottom-[20px] '>
-                            <div className='bg-gray-300 px-2 rounded capitalize'>
-                                {course.level}
+                        !user.is_superuser && !user.is_provider ? 
+                        <>
+                            <div className='absolute w-full flex justify-between px-5 bottom-[35px] '>
+                                <p className='bg-gray-300 px-2 py-1 uppercase text-xs'>
+                                    {course.level}
+                                </p>
+                                <FontAwesomeIcon icon={faStar} className={`${ is_favorite ? 'text-yellow-500 hover:text-yellow-300' : 'text-gray-300 hover:text-gray-600' } hover:cursor-pointer`} title='Favorite' onClick={toggleWishList}/>
                             </div>
-                        </div>
-                        :
-                         
+                            {purchaseDetails ? <div className='text-xs text-[#c4c4c4] font-semibold w-full text-center absolute bottom-[10px]'>Started {getDate(purchaseDetails.purchase_date)}</div>: null }
+                        </> 
+                        : 
                             <div className='absolute b-[10px] w-full flex justify-center gap-4 bottom-[20px] '>
                                         {
                                             is_admin ? 
-                                            <button className='w-[120px] py-1 text-center bg-[#4B527E] rounded text-white'
-                                            onClick={handleApprove}
-                                            >
-                                            Approve
-                                            </button>
+                                            (
+                                                course.is_published ?
+                                                <button className='w-[120px] py-1 text-center bg-[#4B527E] rounded text-white' onClick={handleBlock}>Block</button>
+                                                :
+                                                <button className='w-[120px] py-1 text-center bg-[#4B527E] rounded text-white' onClick={handleApprove}>Approve </button>
+                                            )
                                             :
                                         (
                                             course.is_published ? 
-                                            <button className='w-[120px] py-1 text-center bg-[#4B527E] rounded text-white'    
-                                            >
-                                            Unpublish
-                                            </button>
+                                            <button className='w-[120px] py-1 text-center bg-[#4B527E] rounded text-white'   >Unpublish</button>
                                             :
                                             (course.is_pending ? 
-                                                <button className='w-[120px] py-1 text-center bg-gray-300 rounded text-white' disabled
-                                                
-                                                >
-                                                Pending
-                                                </button>
+                                                <button className='w-[120px] py-1 text-center bg-gray-300 rounded text-white' disabled> Pending</button>
                                                 :
-                                                <button className='w-[120px] py-1 text-center bg-[#4B527E] rounded text-white' 
-                                                onClick={handlePublish}
-                                                >
-                                                Publish
-                                                </button>
+                                                <button className='w-[120px] py-1 text-center bg-[#4B527E] rounded text-white' onClick={handlePublish}> Publish </button>
 
                                             )
 
                                         )
                                         }
-                                <button className='w-[120px] py-1 text-center bg-[#E5C3A6] rounded'
-                                onClick={()=>{
-                                    console.log('edit coures clicked')
-                                    setModal(true)
-                                }}
-                                >Edit</button>
+                                <button className='w-[120px] py-1 text-center bg-[#E5C3A6] rounded' onClick={()=>{setModal(true)}}>Edit</button>
                             </div>
                     }
-            {modal && <CustomModal component={<AddCourse course={course} edit="true" getModal={getModal} />} />}
-        </div>
+            {modal && <CustomModal component={<AddCourse course={course} edit="true" getModal={getModal} />} />}  
     </>
   )
 }
--- src/pages/partials/CourseDetails.jsx
+++ src/pages/partials/CourseDetails.jsx
@@ -10,26 +10,43 @@ import Curriculum from './Curriculum'
 import api from '../../api/api'
 import useUrlHeader from '../../utilities/urlHeader'
 import conis from '../../assets/coins.png'
+import CustomModal from '../customs/CustomModal'
+import {Elements} from '@stripe/react-stripe-js';
+import {loadStripe} from "@stripe/stripe-js/pure";
+import CourseCheckout from '../students/components/CourseCheckout'
+import { Rating, Typography } from '@mui/material'
+import AddReview from '../students/components/AddReview'
+import { useSelector } from 'react-redux'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPenToSquare, faTrash } from '@fortawesome/free-solid-svg-icons'
 
 
+const stripePromise = loadStripe('pk_test_51OUSTKSD35HDNE0LxLa3ZdB7RR5jlH2rxFwoyw8neo08LQV9DhA0DsAE6NOwTjcjbKKY3NocOK22hhuo1BL3OQ5A00i5cf2Mc1');
 
 function CourseDetails(props) {
     const { course_id } = useParams()
+    const user = useSelector(state => state.user.value)
     const { is_provider } = props
     const [isDescription, setIsDescription] = useState(true);
     const [course, setCourse] = useState({});
     const [provider, setProvider] = useState({});
     const [subject, setSubject] = useState('');
     const [lesson, setLesson] = useState({});
-    const [is_purchased, setIs_purchased] = useState(false);
+    const [is_purchased, setIs_purchased] = useState(null);
+    const [noEnrolls, setNoEnrolls] = useState(0);
     const auth = useUrlHeader()
+    const [modal, setModal] = useState(false);
+    const [reviewModal, setReviewModal] = useState(false);
+    const [clientSecret, setClientSecret] = useState("");
+    const currentDateTime = new Date();
 
+    const [allReviews, setAllReviews] = useState([]);
     useEffect(() => {
       api.get(
         `/student/check_purchase/${course_id}`,
         auth
       ).then((response) => {
-        setIs_purchased(response.data.is_purchased)
+        setIs_purchased(response.data)
       }).catch((error) => {
         console.log(error);
       })
@@ -41,9 +58,20 @@ function CourseDetails(props) {
         setSubject(response.data.subject)
         setCourse(response.data.course)
         setLesson(response.data.lesson)
+        setNoEnrolls(response.data.no_enrolls)
+        setAllReviews(response.data.reviews)
       })
+      if (!is_provider) {
+        api.post(
+          `/payments/save-stripe-info/`,
+          {course : course_id},
+          auth
+        ).then(response => setClientSecret(response.data.clientSecret))
+      }
     }, []);
     
+    const appearance = {theme: 'stripe',};
+    const options = {clientSecret,appearance,};
     useEffect(() => {
       
     }, [course]);
@@ -62,61 +90,159 @@ function CourseDetails(props) {
         console.log(error);
       })
     }
+    const getModal = (bool) => {
+      setModal(bool)
+      setReviewModal(bool)
+     }
+    const handleReviewDelete = (review_id) =>{
+      console.log('something happened');
+      api.delete(
+        `/student/review-rating/${review_id}`,
+        
+        auth
+      ).then((response) => {
+        console.log(response.data);
+        const updatedReviews = allReviews.filter(review => review.id !== 2);
+        setAllReviews(updatedReviews)
+      }).catch(error => {
+        console.log(error);
+      })
+    }
+    
+
     return (
-    <div className={ is_provider ? 'p-10 flex border-4 border-red-500 flex h-screen' : 'px-36 py-1 pb-10 border-4 border-green-500 flex h-screen' }>
-      <div className='w-3/4 overflow-scroll p-3'>
-      {console.log(course)}
-          <h1 className='font-bold text-[40px]'>{course.name} </h1>
-          <p className='text-[24px] mt-2 p-3'>{course.short_description}</p>
-          <div className='mt-3 flex gap-4 items-center'>
-            <img src={accPic} alt="avatar" className='w-10 h-10 rounded-full' />
-            <div className=''>
-              <p className='text-gray-500 font-semibold'>Provider</p>
-              <p>{provider.username}</p>
+    <div className={ is_provider || user.is_superuser ? 'p-3 sm:p-10 h-screen sm:p-5 lg:flex' : 'p-1 sm:p-5 lg:px-36 py-1 pb-10 lg:flex h-screen' }>
+      <div className='w-full lg:w-3/4 overflow-scroll p-3'>
+        
+          <h1 className='font-bold text-[24px] sm:text-[28px] lg:text-[40px]'>{course.name} </h1>
+          <p className='text-[15px] sm:text-[18px] lg:text-[24px] p-2 sm:mt-2 sm:p-3'>{course.short_description}</p>
+          <div className='flex justify-evenly mt-3 lg:block lg:mt-0'>
+            <div className='mt-1 lg:mt-3 flex gap-4 items-center'>
+              <img src={accPic} alt="avatar" className='w-10 h-10 rounded-full' />
+              <div className=''>
+                <p className='text-gray-500 font-semibold'>Provider</p>
+                <p>{provider.username}</p>
+              </div>
+            </div>
+            <div className='mt-1 lg:mt-5 flex gap-4 items-center'>
+              <img src={saved} alt="" className='w-8 h-8 ' />
+                <div>
+                  <p className='text-gray-500 font-semibold'>Subject</p>
+                  <p className='capitalize'>{subject}</p>
+                </div>
             </div>
           </div>
-          <div className='mt-5 flex gap-4 items-center'>
-            <img src={saved} alt="" className='w-8 h-8 ' />
-              <div>
-                <p className='text-gray-500 font-semibold'>Subject</p>
-                <p className='capitalize'>{subject}</p>
-              </div>
-          </div>
           <div className='bg-[#f0f4fa] flex justify-evenly mt-6 '>
-              <div onClick={()=>setIsDescription(true)} className={isDescription ? 'bg-white border-t-4 border-[#2E4374] p-4 w-full text-center font-semibold hover:bg-[#EEEEEE]' : 'p-4 w-full text-center font-semibold hover:bg-[#EEEEEE]' }>
+              <div onClick={()=>setIsDescription(true)} className={isDescription ? 'bg-white border-t-4 border-[#2E4374] p-2 lg:p-4 w-full text-center font-semibold hover:bg-[#EEEEEE]' : 'p-2 lg:p-4 w-full text-center font-semibold hover:bg-[#EEEEEE]' }>
                 <p>Description</p>
               </div>
-              <div onClick={()=>{setIsDescription(false)}} className={isDescription ? 'p-4 w-full text-center font-semibold hover:bg-[#EEEEEE]' : 'bg-white border-t-4 border-[#2E4374] p-4 w-full text-center font-semibold hover:bg-[#EEEEEE]' }>
+              <div onClick={()=>{setIsDescription(false)}} className={isDescription ? 'p-2 lg:p-4 w-full text-center font-semibold hover:bg-[#EEEEEE]' : 'bg-white border-t-4 border-[#2E4374] p-2 lg:p-4 w-full text-center font-semibold hover:bg-[#EEEEEE]' }>
                 <p>Curriculum</p>
               </div>
           </div>
           { isDescription ? <Description course={course} provider={provider}/> : <Curriculum course={course} is_provider={is_provider}/> }
+
+          
+          <div className='mt-20 m-2'>
+            <h1 className='my-10 flex justify-between items-center'><span className='text-[24px]'>Review and rating</span><span onClick={()=>setReviewModal(true)} className='me-5 px-3 py-1 bg-yellow-300 text-white rounded hover:bg-yellow-200 hover:cursor-pointer'>Add review</span></h1>
+              
+                {
+                  reviewModal && <CustomModal component={ <AddReview course_id={course.id} getModal={getModal} /> } />
+                }
+                {allReviews.length === 0 && <p>No reviews yet</p>}
+            {allReviews.map(review => {
+              return (
+                <div key={review.id} className='mt-4'>
+                  <div className='flex justify-between gap-3'>
+                    <div className='flex gap-3'>
+                      <img src={review.student.avatar ? review.student.avatar : accPic} alt="avatar" className='w-10 h-10 rounded-full' />
+                      <p>{review.student.username}</p>    
+                    </div>
+                    {
+                      review.student.id == user.user_id && <>
+                        <div className='me-4 flex gap-5'>
+                          <FontAwesomeIcon icon={faPenToSquare} className='text-blue-600 hover:text-blue-400'/>
+                          <FontAwesomeIcon icon={faTrash} onClick={()=>{handleReviewDelete(review.id)}} className='text-red-600 hover:text-red-400' />
+                        </div>
+                      </> 
+                    }
+                  </div>
+                  <div className='mx-[40px] '>
+                    <div className='flex items-center gap-4'>
+                      <Rating className='my-2' name="read-only" value={review.rate} readOnly /> 
+                      <span className='text-gray-400'> {new Date(review.added_date).toLocaleDateString()}</span>
+                    </div>
+                    <p className='pb-6 border-b-2'>{review.review}</p>
+
+                  </div>
+                </div>
+
+              )
+            })}
+
+
+
+          </div>
       </div>
-      <div className='w-1/4 '>
+
+
+      <div className='w-full lg:w-1/4 overflow-y-scroll'>
         { 
           is_provider ? 
           null : 
           (
             is_purchased ?
+            <>
+              
+              
+              
+              
+              
+              <div className='px-5 py-2  w-full'>
+              {console.log((currentDateTime.toISOString() > is_purchased.end_date) || !is_purchased.is_paid )}
+              
+              {
+                ((currentDateTime.toISOString() < is_purchased.end_date) || is_purchased.is_paid ) ?
+                    <Link to={`/student/course/${course.id}/lesson/${lesson.id}`}>
+                      <button className='p-3 bg-[#2E4374] text-white font-bold border-4 border-double w-full transition  hover:bg-[#54668d] '>Open Course</button>
+                    </Link>
+                  :
+                    <button className='p-3 bg-[#2E4374] text-white font-bold border-4 border-double w-full transition  hover:bg-[#54668d] cursor-not-allowed'>Course Expired</button>
+              }
+              </div>
+              {
+                is_purchased.is_paid ?
+                null :
+              <div onClick={()=>{setModal(true)}} className='flex items-center cursor-pointer px-2 mx-5 border-4 bg-[#efdbc9] hover:bg-[#F3E5D9] border-[#F3E5D9] hover:border-double hover:border-[#efdbc9]'>
+                  {console.log(is_purchased.is_paid)}
+                  <img src={conis} className='h-[55px]' />
+                  <p className='text-[15px]'>Upgrade course for ${course.course_price}</p>
+              </div>
+
+              }
+             {modal && clientSecret &&<CustomModal component={<Elements stripe={stripePromise} options={options}><CourseCheckout getModal={getModal} course={course}/></Elements>} />} 
             
-            <div className='p-5'>
-              <Link to={`/student/course/${course.id}/lesson/${lesson.id}`}>
-                <button className='px-3 py-2 bg-blue-400 rounded'>Open Course</button>
-              </Link>
-            </div>
+            
+            
+            
+            
+            
+            </>
             :
+            <>
             <div className='p-5'>
-              <button className='px-3 py-2 bg-blue-400 rounded' onClick={handleEnroll}>Enroll Course</button>
+              <button className='p-3 bg-[#24355c] text-white font-bold border-4 border-double w-full transition hover:bg-[#39496c]' onClick={handleEnroll}>Enroll Course</button>
             </div>
+            <div className='flex items-center py-1 px-2 mx-5 border-b-2 border-[#ececee] bg-[#efdbc9]'>
+                <img src={conis} className='h-[55px]' />
+                <p className='text-[15px]'>Upgrade Available for ${course.course_price}</p>
+            </div>
+            </>
           )
         }
         <div className='px-5'>
-          <div className='flex items-center py-1 px-2 border-b-2 border-[#ececee] bg-[#efdbc9]'>
-              <img src={conis} className='h-[55px]' />
-              <p className='text-[15px]'>Upgrade {is_purchased ? <>course</> : <>available</>} for ${course.course_price}</p>
-          </div>
           <div className='flex justify-between items-center py-5 px-2 border-b-2 border-[#ececee]'>
-            <p>Enrolled : <span className='font-bold'> 100 students </span> </p>
+            <p>Enrolled : <span className='font-bold'> {noEnrolls} students </span> </p>
             <img src={students} alt="studs" className='w-8 h-8'/>
           </div>
           <div className='flex justify-between items-center py-5 px-2 border-b-2 border-[#ececee]'>
@@ -129,13 +255,6 @@ function CourseDetails(props) {
           </div>
         </div>
         <div className='px-2 border-2 border-[#ececee]'>
-          <div className='flex gap-3 p-3'>
-            <img src="https://picsum.photos/seed/picsum/200/300" className='w-1/3 h-20' />
-            <div className='w-2/3'>
-              <p className='line-clamp-2 font-semibold'>I am a mad super crazy scientists</p>
-              <p className='text-slate-400 text-[15px]'>By Albert Einstein</p>
-            </div>
-          </div>
           <div className='flex gap-3 p-3'>
             <img src="https://picsum.photos/seed/picsum/200/300" className='w-1/3 h-20' />
             <div className='w-2/3'>
