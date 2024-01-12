76bc7d0aa7a1614be6fa53ce401ad88aca6403b1
Hamdan Salih <hamdansalihpc@gmail.com>
2024-01-12 15:53:04 +0530

    curriculum description updated

--- src/pages/partials/Curriculum.jsx
+++ src/pages/partials/Curriculum.jsx
@@ -2,23 +2,27 @@ import React, { useEffect, useState } from 'react'
 import time from '../../assets/time.png'
 import play from '../../assets/play.png'
 import api from '../../api/api'
-import useUrlHeader from '../../utilities/urlHeader'
 import CustomModal from '../customs/CustomModal'
 import AddLesson from '../providers/components/AddLesson'
 import deletePic from '../../assets/delete.png'
-import { Link } from 'react-router-dom'
-
+import { Link, useNavigate } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faClipboard, faPlus, faPlusCircle, faTrash } from '@fortawesome/free-solid-svg-icons'
+import AddQuiz from '../providers/components/AddQuiz'
 
 function Curriculum(props) {
     const [module, setModule] = useState('');
     const { course , is_provider } = props
-    const auth = useUrlHeader()
     const [curriculum, setCurriculum] = useState([]);
     const [lessonModal, setLessonModal] = useState(false);
     const [moduleId, setModuleId] = useState(null);
+    const [QuizModal, setQuizModal] = useState(false);
+    const [quiz, setQuiz] = useState({});
+    const navigate = useNavigate()
     
     const getModal = (bool) => {
         setLessonModal(bool)
+        setQuizModal(bool)
     }
     useEffect(() => {
         console.log(curriculum);
@@ -27,22 +31,24 @@ function Curriculum(props) {
     useEffect(() => {
         api.get(
             `/provider/getCurriculumDetails/${course.id}`,
-            auth
         ).then(response => {
             console.log(response.data);
             setCurriculum(response.data)
         }).catch(error => {
             console.log(error);
         })
+        api.get(`/provider/quiz/?course_id=${course.id}`).then(response => {
+            setQuiz(response.data)
+            console.log(response.data);
+        }).catch((error)=>console.log(error))
     }, [lessonModal]);
-
     
     const handleAddModule = ()=>{
         console.log(course.id);
         api.post(
             '/provider/addModule/',
             {name : module , course : course.id},
-            auth
+            
             ).then((response)=>{
                 if (response.status === 201) {
                     console.log(response.data);
@@ -61,7 +67,7 @@ function Curriculum(props) {
             const id = e.target.name       
             api.delete(
             `/provider/delete_module/${id}`,
-            auth
+            
             ).then(response => {
                 console.log('Module Deleted successfully');
                 console.log(curriculum);
@@ -77,7 +83,7 @@ function Curriculum(props) {
             const id = e.target.name
             api.delete(
                 `/provider/delete_lesson/${id}`,
-                auth
+                
                 ).then(response => {
                     console.log('Module Deleted successfully');
                     console.log(curriculum);        
@@ -93,35 +99,55 @@ function Curriculum(props) {
   return (
     <>
 
-    <div className='my-10 px-8 grid '>
-        <div className='flex w-full'>
+    <div className='lg:my-10 lg:px-8 grid '>
+        <div className=' sm:flex w-full gap-5 h-[40px] items-center'>
             {
                 is_provider ?
-            <div className='ml-0 ml-auto w-2/3'> 
-                <input type="text" className='w-3/4' name="module" value={module} onChange={(e)=>setModule(e.target.value)}/>
-                <button onClick={handleAddModule} className='bg-[#4B527E] w-1/4 text-white px-3 py-2 rounded-e hover:bg-[#7C81AD] hover:shadow-lg'>Add Module</button>
-            </div>
+                <>
+                    <div className='w-full sm:w-2/3 h-full flex'> 
+                        <input type="text" className='w-3/4 h-full' name="module" value={module} onChange={(e)=>setModule(e.target.value)}/>
+                        <button onClick={handleAddModule} className='bg-[#4B527E] w-1/4 h-full text-white px-3 py-2 rounded-e hover:bg-[#7C81AD] hover:shadow-lg'>Add Module</button>
+                    </div>
+                    <button onClick={()=>{setQuizModal(true);console.log(QuizModal)}} className='w-full mt-3 sm:mt-0 sm:w-1/4 h-full bg-gradient-to-t from-[#00D775] to-[#00BD68] rounded shadow-[rgba(0, 0, 0, 0.1) 0 2px 4px] text-white cursor-pointer flex justify-center items-center gap-3  outline-none overflow-hidden px-[12px] hover:bg-[#00bd68]'>
+                        <FontAwesomeIcon icon={faPlusCircle}  className='text-white '/>
+                        <p>Qiuz</p>
+                    </button>
+                    {QuizModal && <CustomModal component={<AddQuiz getModal={getModal} is_update={false} course_id={course.id} /> } /> }
+                </>
                 :
             
             null
             }
         </div>
-        <div className='mt-5'>
+        
+        <div className='lg:mt-5'>
+        {
+            Object.keys(quiz).length > 0 && is_provider ? 
+                <div onClick={()=>navigate('quiz')} className=' mt-16 sm:mt-6 w-full bg-[#7C81AD] py-3 px-5 rounded flex justify-between items-center cursor-pointer hover:bg-[#898db5]'>
+                    {console.log(quiz)}
+                    <div className='flex items-center gap-4 text-white'>
+                        <FontAwesomeIcon icon={faClipboard} />
+                        <p>Quiz</p>
+                    </div>
+                    <FontAwesomeIcon icon={faTrash} className='text-red-500 hover:text-red-700'/>
+                </div>
+            : null
+        }
 
             {curriculum.map((module , index) => {
                 return (
                     <div key={index}>
-                        <div className='flex items-center pr-4 bg-[#D8DBE1] ps-5 mt-3 rounded'>
-                            <h1 className='text-[24px] my-4 font-semibold'>{module.module.name}</h1>
+                        <div className='flex items-center bg-[#D8DBE1] px-5 mt-3 rounded'>
+                            <h1 className='text-[19px] lg:text-[24px] my-4 font-semibold'>{module.module.name}</h1>
                             { is_provider ? <button name={module.module.id} onClick={handleDelete} className='ml-0 ml-auto bg-red-500 text-white px-3 rounded py-2'>Delete</button> : null }  
                         </div>
                         {module.lessons.map((lesson , index) => {
                             return (
                                 <div key={lesson.id} >
-                                    <div className='flex gap-5 items-center w-full p-4 mt-1 bg-[#f0f4fa]'>
+                                    <div className='flex gap-2 lg:gap-5 items-center w-full p-4 mt-1 bg-[#f0f4fa]'>
                                         <p className='text-slate-400 font-bold'>{index + 1}</p>
                                         <img src={play} alt="play" className='w-5 h-5'/>
-                                        <h2 className='capitazlie'>
+                                        <h2 className='capitazlie truncate'>
                                             { 
                                                 is_provider ?
                                                 <Link to={`/provider/course/${course.id}/lesson/${lesson.id}`}>{lesson.name}</Link>
@@ -152,44 +178,7 @@ function Curriculum(props) {
             })}
 
             {lessonModal && <CustomModal component={<AddLesson getModal={getModal} is_update={false} module_id={moduleId}/>} /> }
-
-            {/* <div>
-                <h1 className='text-[24px] my-4'>Module 1</h1>
-                <div>
-                    <div className='flex gap-5 items-center w-full p-4 mt-1 bg-[#f0f4fa]'>
-                        <p className='text-slate-400 font-bold'>1</p>
-                        <img src={play} alt="play" className='w-5 h-5'/>
-                        <h2>Introduction</h2>
-                        <div className='flex gap-4 px-2 ml-0 ml-auto'>
-                            <img src={time} alt="time" className='w-5 h-5'/>
-                            <span> 10:20</span>
-                        </div>
-                    </div>
-                    <div className='flex gap-5 items-center w-full p-4 mt-1 bg-[#f0f4fa]'>
-                        <p className='text-slate-400 font-bold'>1</p>
-                        <img src={play} alt="play" className='w-5 h-5'/>
-                        <h2>Introduction</h2>
-                        <div className='flex gap-4 px-2 ml-0 ml-auto'>
-                            <img src={time} alt="time" className='w-5 h-5'/>
-                            <span> 10:20</span>
-                        </div>
-                    </div>
-                    <div className='flex gap-5 items-center w-full p-4 mt-1 bg-[#f0f4fa]'>
-                        <p className='text-slate-400 font-bold'>1</p>
-                        <img src={play} alt="play" className='w-5 h-5'/>
-                        <h2>Introduction</h2>
-                        <div className='flex gap-4 px-2 ml-0 ml-auto'>
-                            <img src={time} alt="time" className='w-5 h-5'/>
-                            <span> 10:20</span>
-                        </div>
-                    </div>
-                </div>
-            </div> */}
-                
-           
         </div>
-
-
     </div>
     </>
   )
--- src/pages/partials/Description.jsx
+++ src/pages/partials/Description.jsx
@@ -9,28 +9,28 @@ function Description(props) {
     }, [course]);
     return (
         <div className='mt-10 '>
-            <div className='px-8 pb-10 border-b-2 border-[#2E4374] flex justify-center'>
-                <video controls src={course.course_preview} className='p-4  w-[600px]'></video>
+            <div className='lg:px-8 pb-10 border-b-2 border-[#2E4374] flex justify-center'>
+                <video controls src={course.course_preview} className='lg:p-4 w-[600px]'></video>
             </div>
             <div className='p-3'>
-                <h1 className='py-3 text-[28px]'>Description</h1>
-                <p className='text-[19px]'>{course.description ?<> {course.description.split('\n').map((paragraph, index) => <React.Fragment key={index}>{paragraph}<br /></React.Fragment>)}</> : course.description}</p>
+                <h1 className='py-1 font-semibold lg:py-3 text-[24px] lg:text-[28px]'>Description</h1>
+                <p className='text-[16px] lg:text-[19px]'>{course.description ?<> {course.description.split('\n').map((paragraph, index) => <React.Fragment key={index}>{paragraph}<br /></React.Fragment>)}</> : course.description}</p>
 
-                <h1 className='py-3 text-[28px]'>About</h1>
-                <p className='text-[19px]'>{ course.about ? <>{course.about.split('\n').map((paragraph, index) => <React.Fragment key={index}>{paragraph}<br /></React.Fragment>)}</> : course.about }</p>
+                <h1 className='mt-3 py-1 font-semibold lg:py-3 text-[24px] lg:text-[28px]'>About</h1>
+                <p className='text-[16px] lg:text-[19px]'>{ course.about ? <>{course.about.split('\n').map((paragraph, index) => <React.Fragment key={index}>{paragraph}<br /></React.Fragment>)}</> : course.about }</p>
                 
-                <h1 className='py-3 text-[28px]'>Prequisites</h1>
-                <p className='text-[19px]'>{ course.prerequisites ? <>{course.prerequisites.split('\n').map((paragraph, index) => <React.Fragment key={index}>{paragraph}<br /></React.Fragment>)}</> : course.prerequisites }</p>
+                <h1 className='mt-3 py-1 font-semibold lg:py-3 text-[24px] lg:text-[28px]'>Prequisites</h1>
+                <p className='text-[16px] lg:text-[19px]'>{ course.prerequisites ? <>{course.prerequisites.split('\n').map((paragraph, index) => <React.Fragment key={index}>{paragraph}<br /></React.Fragment>)}</> : course.prerequisites }</p>
             </div>
 
-            <div className='flex w-ful mt-10'>
-                <div className='w-1/4'>
-                    <img src={accPic} alt="provider" className='shadow-md'/>
+            <div className='flex items-center gap-4 w-ful mt-10 px-6'>
+                <div className='w-1/4 h-[160px] lg:h-[200px] border border-2'>
+                    <img src={provider.avatar ? `${provider.avatar}` : accPic} alt="provider" className='shadow-md'/>
                 </div>
-                <div className='w-3/4'>
-                    <h1 className='py-3 text-[28px]'>{(provider.first_name && provider.last_name) ? <>{provider.first_name} { provider.last_name }  </> : 'Not added' }</h1>
-                    <p>
-                        This will be the profile display of provider to add
+                <div className='w-3/4 h-[160px] lg:h-[200px] overflow-y-scroll'>
+                    <h1 className='py-3 text-[16px] md:text-[20px] lg:text-[28px] capitalize'>{(provider.first_name && provider.last_name) ? <>{provider.first_name} { provider.last_name }  </> : 'Not added' }</h1>
+                    <p className='text-[13px] md:text-[16px]'>
+                        {provider.about_me}
                     </p>
                 </div>
             </div>
