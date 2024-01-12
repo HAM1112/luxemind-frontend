f377da66bf2b162ad0848cd4e4b3015bf88b86c4
Hamdan Salih <hamdansalihpc@gmail.com>
2024-01-12 15:54:13 +0530

    menbar navbar

--- src/pages/partials/Menubar.jsx
+++ src/pages/partials/Menubar.jsx
@@ -8,13 +8,16 @@ function Menubar(props) {
   const [items, setItems] = useState(props.items);
   {console.log(items)}
   return (
-    <div className='menubar sticky h-screen py-5  hidden md:block'>
+    <div className='menubar sticky z-0 md:h-screen md:py-5 md:block'>
       <div className='grid'>
-        <div className='grid gap-2 justify-items-center content-center border-b-2 py-5 mx-5'>
+        <div className='hidden md:grid gap-2 justify-items-center content-center border-b-2 py-5 mx-5'>
             <img src={accoPic} alt="accpic" className='border-2 border-slate-600 rounded-full w-32'/>
             <h1 className='font-bold'>User Name</h1>
         </div>
-        <div className='grid p-5 gap-2 font-semibold capitalize'>
+        <div className='md:hidden'>
+          Provider pages 
+        </div>
+        <div id="dropdownDelay" className='hidden md:grid p-5 gap-2 font-semibold capitalize'>
             {items.map((item , index)=> {
               return <NavLink to={item} key={index}>{item}</NavLink>
             })}
--- src/pages/partials/Navbar.jsx
+++ src/pages/partials/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { NavLink } from 'react-router-dom';
 
 import logo from '../../assets/luxeminds.png'
@@ -6,40 +6,64 @@ import accPic from '../../assets/acco.png'
 import './navabar.css'
 
 function Navbar() {
-  return (
-    <nav className='navbar flex gap-16 shadow-lg h-14 '>
-    
-      <div>
-        <NavLink to="home"><img src={logo} alt="logo" className='h-14 ms-10 py-1'/></NavLink>
-      </div>
-      
-      <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-          <span className="sr-only">Open main menu</span>
-          <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-          </svg>
-      </button>
+    const [isOpen, setIsOpen] = useState(window.innerWidth >= 768); 
+    const [bigWindow, setbigWindow] = useState(window.innerWidth >= 768);
 
-      <div className='flex gap-10 w-full ps-4 hidden md:flex md:w-full' id="navbar-default">
-        <div className='flex justify-center items-center'>
-            <NavLink to="courses">Courses</NavLink>
+    const toggleDropdown = () => {
+      setIsOpen(!isOpen);
+      console.log(isOpen);
+    };
+
+    useEffect(() => {
+      const handleResize = () => {
+        setIsOpen(window.innerWidth >= 768); // Update isOpen based on window width
+        setbigWindow(window.innerWidth >= 768)
+      };
+
+      window.addEventListener('resize', handleResize);
+
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      };
+    }, []);
+  return (
+    <>
+      <nav className='navbar relative fixed top-0 w-full bg-white flex gap-16 shadow-lg h-12 sm:h-14'>
+      
+        <div>
+          <NavLink to="home"><img src={logo} alt="logo" className='h-12 sm:h-14 ms-3 sm:ms-10 py-1'/></NavLink>
         </div>
-        <div className='flex justify-center items-center'>
-            <NavLink to="#">Providers</NavLink>
+        
+        <button type="button" onClick={toggleDropdown} className="inline-flex absolute end-3 top-1  items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+            <span className="sr-only">Open main menu</span>
+            <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+            </svg>
+        </button>
+        {console.log((isOpen && !bigWindow))}
+        <div className={` ${(isOpen && !bigWindow) ? 'absolute bg-white w-[250px] border-gray-300 border-2 grid top-14 right-1 bg-[#eeeeee] p-2' : 'hidden' } gap-3 md:gap-10 w-full ps-4  md:flex md:w-full`} id="navbar-default">
+          <div className='flex justify-center items-center'>
+              <NavLink to="courses">Courses</NavLink>
+          </div>
+          <div className='flex justify-center items-center'>
+              <NavLink to="#">Leaderboard</NavLink>
+          </div>
+          <div className='flex justify-center items-center'>
+              <NavLink to="#">Discover New</NavLink>
+          </div>
+          <div className={`${(isOpen && !bigWindow) ? 'hidden' : null}  md:flex-grow`}></div>
+          <div className='flex justify-center items-center'>
+              <NavLink to="#">Help</NavLink>
+          </div>
+          <div className='flex justify-center items-center gap-3 bg-gray-300 px-7'>
+              <img src={accPic} alt="" className='h-8 w-8 rounded-full'/>
+              <NavLink to="profile">Profile</NavLink>
+          </div>
         </div>
-        <div className='flex justify-center items-center'>
-            <NavLink to="#">Discover New</NavLink>
-        </div>
-        <div className='flex-grow'></div>
-        <div className='flex justify-center items-center'>
-            <NavLink to="#">Help</NavLink>
-        </div>
-        <div className='flex justify-center items-center gap-3 bg-gray-300 px-7'>
-            <img src={accPic} alt="" className='h-8 w-8 rounded-full'/>
-            <NavLink to="profile">Profile</NavLink>
-        </div>
-      </div>
-    </nav>
+
+      </nav>
+      <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
+    </>
   )
 }
 
