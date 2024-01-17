6c037ed3c3a59837cd37c0ab642bce02c5b71a49
Hamdan Salih <hamdansalihpc@gmail.com>
2024-01-17 16:21:43 +0530

    savedlessons in profile

---
+++ src/pages/students/components/SavedLesson.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function SavedLesson() {
+  return (
+    <div className='w-full'>
+          <h1 className='font-bold py-5 text-3xl w-full '>Saved Lessons</h1>
+          <div className='flex gap-4'>
+            <div className='border-2 border-[#E5C3A6] w-1/4 sm:w-1/6 md:w-1/12'></div>
+            <div className='border-2 border-gray-200 w-3/4 sm:w-5/6 md:w-11/12'></div>
+          </div>
+        </div>
+  )
+}
+
+export default SavedLesson
